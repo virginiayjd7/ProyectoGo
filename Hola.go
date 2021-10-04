c913179b6bdd58e1a9dbdffe49346d7742e4b902
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2021-10-04 18:34:50 -0500

    go

---
+++ Hola.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", homeHandler)
+
+	http.ListenAndServe(":3000", nil)
+
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hola"))
+}
